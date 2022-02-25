db00bbcb8b016415dbcc0bfaa0e38449e0fb5f63
hailm <hai.lemanh.1310@gmail.com>
2022-02-25 22:16:14 +0700

    update block01

--- index.php
+++ index.php
@@ -1,5 +1,143 @@
 <?php $data["title"] = "Trang chủ"; ?>
 <?php $body = '' ?>
 <?php require "template-parts/layouts/header.php"; ?>
+<div class="uk-position-relative">
+    <div class="home__block01 uk-flex uk-flex-middle uk-background-center-center uk-background-norepeat" data-src="images/1x/bg1.png" uk-img>
+        <div class="home__block01__section uk-section uk-width-1-1">
+            <div class="uk-container">
+                <div class="uk-flex-middle" uk-grid>
+                    <div class="uk-width-1-3@m">
+                        <div class="home__block01__boxitem1 uk-text-center uk-text-left@m">
+                            <div class="home__block01__title">
+                                ĐẠI HỌC FPT <br class="uk-visible@m">
+                                TUYỂN SINH <br class="uk-visible@m">
+                                BỔ SUNG <br class="uk-visible@m">
+                                1000 CHỈ TIÊU</div>
+                            <div class="home__block01__txt">Hạn nhập học 15/10</div>
+                        </div>
+                    </div>
+                    <div class="uk-width-expand uk-visible@m">
 
+                    </div>
+                    <div class="uk-width-1-3@m">
+                        <div class="home__block01__card uk-card uk-card-body uk-card-default">
+                            <form class="home__block01__form">
+                                <fieldset class="uk-fieldset">
+
+                                    <legend class="uk-legend uk-text-center home__block01__form__title">ĐĂNG KÝ TƯ VẤN</legend>
+
+                                    <div class="uk-margin">
+                                        <input class="uk-input home__block01__form__input" type="text" placeholder="Họ và tên">
+                                    </div>
+                                    <div class="uk-margin">
+                                        <input class="uk-input home__block01__form__input" type="text" placeholder="Điện thoại">
+                                    </div>
+                                    <div class="uk-margin">
+                                        <input class="uk-input home__block01__form__input" type="text" placeholder="Email">
+                                    </div>
+                                    <div class="uk-margin">
+                                        <input class="uk-input home__block01__form__input" type="text" placeholder="Tỉnh / Thành phố">
+                                    </div>
+                                    <div class="uk-margin-top uk-text-center">
+                                        <button class="home__block01__form__btn uk-button uk-button-secondary uk-border-pill"><span>ĐĂNG KÝ</span></button>
+                                    </div>
+
+                                </fieldset>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="uk-position-top home__header">
+        <div class="uk-container">
+            <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
+                <div class="uk-navbar-left uk-visible@m">
+                    <a href="" class="uk-navbar-item uk-logo uk-background-default"><img src="images/1x/logo.png" alt=""></a>
+                </div>
+                <div class="uk-navbar-center uk-hidden@m">
+                    <a href="" class="uk-navbar-item uk-logo uk-background-default"><img src="images/1x/logo.png" alt=""></a>
+                </div>
+                <div class="uk-navbar-right">
+                    <a class="uk-navbar-toggle uk-hidden@m" href="#">
+                        <span uk-navbar-toggle-icon></span> <span class="uk-margin-small-left">Menu</span>
+                    </a>
+                    <ul class="uk-navbar-nav uk-visible@m">
+                        <li><a href="#">TRANG CHỦ</a></li>
+                        <li><a href="#">TẠI SAO CHỌN ĐH FPT?</a></li>
+                        <li><a href="#">THÔNG BÁO TUYỂN SINH</a></li>
+                        <li><a href="#">CHUYÊN NGÀNH ĐÀO TẠO</a></li>
+                        <li><a href="#">ĐĂNG KÝ HỌC</a></li>
+                        <li><a href="#">ĐĂNG NHẬP</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+</div>
+<div class="uk-section">
+    <div class="uk-container">
+        <div class="uk-section-xsmall">
+            <div class="uk-flex-middle" uk-grid>
+                <div class="uk-width-expand">
+                    <div class="uk-flex-center" uk-grid>
+                        <div class="uk-width-1-1@m uk-width-1-2">
+                            <div class="uk-text-center">
+                                <div>5</div>
+                                <div>CHẤT LƯỢNG ĐÀO TẠO</div>
+                            </div>
+                        </div>
+                        <div class="uk-width-1-1@m uk-width-1-2">
+                            <div class="uk-text-center">
+                                <div>100%</div>
+                                <div>SINH VIÊN LÀM VIỆC THỰC TẾ <br>
+                                    TẠI DOANH NGHIỆP TỪ NĂM 3</div>
+                            </div>
+                        </div>
+                        <div class="uk-width-1-1@m uk-width-1-2">
+                            <div class="uk-text-center">
+                                <div>98%</div>
+                                <div>SINH VIÊN CÓ <br>
+                                    VIỆC LÀM NGAY</div>
+                            </div>
+                        </div>
+                        <div class="uk-width-1-1@m uk-width-1-2">
+                            <div class="uk-text-center">
+                                <div>19%</div>
+                                <div>SINH VIÊN CÓ VIỆC LÀM <br>
+                                    TẠI NƯỚC NGOÀI</div>
+                            </div>
+                        </div>
+                        <div class="uk-width-1-1@m uk-width-1-2">
+                            <div class="uk-text-center">
+                                <div>
+                                    <div class="uk-grid-collapse uk-child-width-auto uk-flex-middle uk-flex-center" uk-grid>
+                                        <div>
+                                            <div>800</div>
+                                        </div>
+                                        <div>
+                                            <div class="uk-text-left">
+                                                SUẤT <br>
+                                                HỌC BỔNG
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>4 NĂM ĐẠI HỌC <br>
+                                    CHO TÂN SINH VIÊN</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="uk-width-auto@m">
+                    <img src="images/1x/img1.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="uk-section-xsmall">
+
+        </div>
+    </div>
+</div>
 <?php require "template-parts/layouts/footer.php"; ?>
